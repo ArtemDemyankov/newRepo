9e329211394b22ef5bbf833f032dac80a23c93ed
Artem Demyankov <artemd98@mail.ru>
2019-03-17 22:02:32 +0300

    Added changes to Main

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Пошел на хуй");
         System.out.println("Пошел");
+        System.out.println("Ну и иди");
     }
 }
